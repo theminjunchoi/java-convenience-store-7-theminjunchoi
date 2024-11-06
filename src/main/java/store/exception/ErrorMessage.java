e1b6cc987ceae819b18aa67cbb8e8e515934db32
MinJun Choi <mj043000@naver.com>
2024-11-06 23:21:15 +0900

    feat: 예외 처리 메시지 생성

---
+++ src/main/java/store/exception/ErrorMessage.java
@@ -0,0 +1,16 @@
+package store.exception;
+
+public enum ErrorMessage {
+    INVALID_ORDER("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+
+    private final static String EXCEPTION_HEADER = "[ERROR] ";
+    private final String message;
+
+    ErrorMessage(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return EXCEPTION_HEADER + message;
+    }
+}
