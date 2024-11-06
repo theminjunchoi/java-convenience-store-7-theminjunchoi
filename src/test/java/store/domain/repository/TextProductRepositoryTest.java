6c300b848358d04f7ae5082401e411ecfa0e93d2
MinJun Choi <mj043000@naver.com>
2024-11-07 00:55:55 +0900

    test: 메모리에서 마크다운으로 저장하는 기능 테스트 코드 작성

--- src/test/java/store/domain/repository/TextProductRepositoryTest.java
+++ src/test/java/store/domain/repository/TextProductRepositoryTest.java
@@ -1,6 +1,7 @@
 package store.domain.repository;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import store.domain.product.Product;
@@ -20,6 +21,25 @@ class TextProductRepositoryTest {
         Product findCola = textProductRepository.findByNameAndPromotion("콜라", Promotion.SOFT_DRINK);
 
         //then
-        Assertions.assertThat(product.getName()).isEqualTo(findCola.getName());
+        assertThat(product.getName()).isEqualTo(findCola.getName());
+    }
+
+    @Test
+    @DisplayName("md 파일에서 products 읽기")
+    void saveMarkdown_Test() {
+        //given
+        textProductRepository.createRepository();
+
+        //when
+        Product hwanta = new Product("환타", 2000, 5, Promotion.SOFT_DRINK);
+        textProductRepository.add(hwanta);
+        textProductRepository.save();
+
+        //then
+        //product.md에 환타 있는 지 확인
+        TextProductRepository newTextProductRepository = new TextProductRepository();
+        newTextProductRepository.createRepository();
+        Product findHwanta = newTextProductRepository.findByNameAndPromotion("환타", Promotion.SOFT_DRINK);
+        assertThat(hwanta.getName().equals(findHwanta.getName()));
     }
 }
