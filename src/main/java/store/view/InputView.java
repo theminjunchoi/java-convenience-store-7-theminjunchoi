e6b07ff8af3afee35c5497aa9074ff7b8f031b39
MinJun Choi <mj043000@naver.com>
2024-11-06 22:09:46 +0900

    refactor: 입력 요구 메시지 변수로 추출

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -3,24 +3,28 @@ package store.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private final static String INPUT_PRODUCT_AND_COUNT = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private final static String INPUT_MORE_PRODUCT_AND_COUNT = "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
+    private final static String INPUT_NOT_DISCOUNT = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)";
+    private final static String INPUT_MEMBERSHIP = "멤버십 할인을 받으시겠습니까? (Y/N)";
+
     public String getProductAndCount() {
-        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        System.out.println(INPUT_PRODUCT_AND_COUNT);
         return Console.readLine();
     }
 
     public String getMoreProductAndCount(String product, int count) {
-//        System.out.println("현재 " + product + "은(는) " + count + "개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
-        System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n", product, count);
+        System.out.printf(INPUT_MORE_PRODUCT_AND_COUNT + "%n", product, count);
         return Console.readLine();
     }
 
     public String getNotDiscount(String product, int count) {
-        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n", product, count);
+        System.out.printf(INPUT_NOT_DISCOUNT + "%n", product, count);
         return Console.readLine();
     }
 
     public String getMembership() {
-        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        System.out.println(INPUT_MEMBERSHIP);
         return Console.readLine();
     }
 }
