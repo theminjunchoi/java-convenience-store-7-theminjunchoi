82ad2945e9694daa54514910dec572b0515ac2fb
MinJun Choi <mj043000@naver.com>
2024-11-07 21:12:17 +0900

    feat: 추가 구매 여부 입력 기능 구현

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
     private final static String INPUT_MORE_PRODUCT_AND_COUNT = "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
     private final static String INPUT_NOT_DISCOUNT = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)";
     private final static String INPUT_MEMBERSHIP = "멤버십 할인을 받으시겠습니까? (Y/N)";
+    private final static String INPUT_EXTRA_PURCHASE = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
 
     public String getProductAndCount() {
         System.out.println(INPUT_PRODUCT_AND_COUNT);
@@ -25,6 +26,11 @@ public class InputView {
 
     public String getMembership() {
         System.out.println(INPUT_MEMBERSHIP);
+        return Console.readLine();
+    }
+
+    public String getExtraPurchase() {
+        System.out.println(INPUT_EXTRA_PURCHASE);
         return Console.readLine();
     }
 }
